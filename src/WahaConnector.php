c2d58e7ba67e426e01f7259742b9213e9da672d9
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-02-22 03:00:32 +0300

    Switch to `HeaderAuthenticator` in WahaConnector for API authentication

--- src/WahaConnector.php
+++ src/WahaConnector.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace NjoguAmos\Waha;
 
 use Saloon\Http\Connector;
-use Saloon\Http\Auth\TokenAuthenticator;
+use Saloon\Http\Auth\HeaderAuthenticator;
 use Saloon\Traits\Plugins\AlwaysThrowOnErrors;
 
 class WahaConnector extends Connector
@@ -15,16 +15,20 @@ class WahaConnector extends Connector
     public function __construct(
         protected string $baseUrl,
         protected string $apiKey,
-    ) {}
+    ) {
+    }
 
     public function resolveBaseUrl(): string
     {
         return $this->baseUrl;
     }
 
-    protected function defaultAuth(): TokenAuthenticator
+    protected function defaultAuth(): HeaderAuthenticator
     {
-        return new TokenAuthenticator(token: $this->apiKey);
+        return new HeaderAuthenticator(
+            accessToken: $this->apiKey,
+            headerName: 'X-API-Key'
+        );
     }
 
     protected function defaultHeaders(): array
