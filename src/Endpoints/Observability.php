f22577d8d09ca1d1a39a0edbf2066e22b583c143
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-03-19 22:28:40 +0300

    Format code

--- src/Endpoints/Observability.php
+++ src/Endpoints/Observability.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace NjoguAmos\Waha\Endpoints;
 
 use NjoguAmos\Waha\Waha;
+use Saloon\Http\Response;
 use Saloon\Exceptions\Request\RequestException;
 use Saloon\Exceptions\Request\FatalRequestException;
 use NjoguAmos\Waha\Requests\Observability\PingRequest;
-use Saloon\Http\Response;
 
 class Observability extends Waha
 {
