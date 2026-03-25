e33f133f228f056954dbac25f7a7c4fba6b5b011
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-03-26 02:47:18 +0300

    Add GetContactRequest and update Contact endpoint and facade with get method

--- src/Facades/Contact.php
+++ src/Facades/Contact.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static Response all(string $sortBy = 'name', string $sortOrder = 'desc', int $limit = 100, int $offset = 0, ?string $session = null)
  * @method static Response exists(string $phone, ?string $session = null)
+ * @method static Response get(string $contactId, ?string $session = null)
  * @method static Response allLids(int $limit = 100, int $offset = 0, ?string $session = null)
  * @method static Response getLid(string $phone, ?string $session = null)
  * @method static Response getAbout(string $contactId, ?string $session = null)
